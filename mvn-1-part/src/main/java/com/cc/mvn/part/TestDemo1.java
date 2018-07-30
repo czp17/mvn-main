0ee3f31cf87cf6da4ca0dc009a6e04d941d2778c
czp17 <771734776@qq.com>
2018-07-30 17:46:25 +0800

    First commit

---
+++ mvn-1-part/src/main/java/com/cc/mvn/part/TestDemo1.java
@@ -0,0 +1,7 @@
+package com.cc.mvn.part;
+
+public class TestDemo1 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ mvn-1-part/src/test/java/com/cc/mvn/part/AppTest.java
@@ -0,0 +1,20 @@
+package com.cc.mvn.part;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
